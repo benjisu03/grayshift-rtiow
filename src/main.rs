5b99a49d196f18f508f84a26236d9e2d43a85cf0
benjisu <benj2003@iastate.edu>
2024-09-24 17:08:57 -0500

    basic image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use indicatif::ProgressIterator;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn Error>>{
+
+    let mut image_file = File::create("image.ppm")?;
+
+    let image_width = 256;
+    let image_height = 256;
+
+    write!(image_file, "P3\n{image_width} {image_height}\n255\n")?;
+
+    for j in (0..image_height).progress() {
+        for i in 0..image_width {
+            let r = (i as f64) / ((image_width - 1) as f64);
+            let g = (j as f64) / ((image_height - 1) as f64);
+            let b = 0.0;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            writeln!(image_file, "{ir} {ig} {ib}")?;
+        }
+    }
+
+    Ok(())
 }
